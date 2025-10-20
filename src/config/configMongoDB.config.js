210239b9f3b957a727b19782a32abf4212bdab79
Facu Janssen <facujanssen@hotmail.com>
2025-10-20 10:54:42 -0300

    cuarto commit

--- src/config/configMongoDB.config.js
+++ src/config/configMongoDB.config.js
@@ -3,8 +3,9 @@ import ENVIRONMENT from "./environment.config.js";
 
 async function connectToMongoDB() {
     try {
-        const connectionString =
-            ENVIRONMENT.MONGO_DB_HOST + "/" + ENVIRONMENT.MONGO_DB_NAME;
+        /* const connectionString =
+            ENVIRONMENT.MONGO_DB_HOST + "/" + ENVIRONMENT.MONGO_DB_NAME; */
+        const connectionString = ENVIRONMENT.MONGODB_URI;
         await mongoose.connect(connectionString);
         console.log("Conexion exitosa a la DB:", mongoose.connection.name);
     } catch (error) {
